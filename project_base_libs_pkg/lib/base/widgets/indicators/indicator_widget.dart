775ee65992564369e8509cab8e57917385cb9eec
YouXianMing <YouXianMing1987@icloud.com>
2022-02-10 10:23:26 +0800

    更新版本号

--- project_base_libs_pkg/lib/base/widgets/indicators/indicator_widget.dart
+++ project_base_libs_pkg/lib/base/widgets/indicators/indicator_widget.dart
@@ -6,7 +6,7 @@ abstract class BaseIndicatorConfig {
   Widget get indicator;
 }
 
-/// 这是一个Indicator的容器类,使用了外观涉及模式,隐藏各个不同的indicator的细节,提供统一的使用接口
+/// 这是一个Indicator的容器类,使用了外观设计模式,隐藏各个不同的indicator的细节,提供统一的使用接口
 class IndicatorWidget<T extends BaseIndicatorConfig> extends StatelessWidget {
   /// indicator的配置
   final T config;
