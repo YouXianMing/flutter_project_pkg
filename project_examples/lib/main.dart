69eff8490072327d327af1e7117375312d7d1528
YouXianMing <YouXianMing1987@icloud.com>
2022-09-09 11:24:07 +0800

    增加启动页

--- project_examples/lib/main.dart
+++ project_examples/lib/main.dart
@@ -7,6 +7,7 @@ import 'package:project_base_libs_pkg/base_file_headers.dart';
 import 'package:project_base_libs_pkg/third_lib_get.dart';
 import 'package:project_examples/route/page_route_enum.dart';
 import 'package:project_examples/app_route_style.dart';
+import 'package:project_examples/widgets/launch_image_widget.dart';
 
 void main() async {
   /// 确保所有的插件都初始化好了
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
           builder: (context, wgt) => GetMaterialApp(
             theme: ThemeData.light(),
             debugShowCheckedModeBanner: false,
-            builder: (context, widget) => widget!,
+            builder: (context, widget) => _widgetMain(context, widget),
             getPages: RouteConfig.getPages(),
             unknownRoute: GetPage(name: '/notFound', page: () => const UnknownPage()),
             initialRoute: '/',
@@ -48,7 +49,7 @@ class MyApp extends StatelessWidget {
           builder: (context, wgt) => GetMaterialApp(
             theme: ThemeData.light(),
             debugShowCheckedModeBanner: false,
-            builder: (context, widget) => widget!,
+            builder: (context, widget) => _widgetMain(context, widget),
             unknownRoute: GetPage(name: '/notFound', page: () => const UnknownPage()),
             routes: {'/': (_) => const TabBarPage()},
             routingCallback: (routing) {
@@ -63,5 +64,19 @@ class MyApp extends StatelessWidget {
           ),
         );
     }
+  }
+
+  Widget _widgetMain(BuildContext context, Widget? widget) {
+    return MediaQuery(
+      // 设置文字大小不随系统设置改变
+      data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+      child: Stack(children: [
+        // 主Widget
+        widget!,
+
+        // 启动页
+        const LaunchImageWidget(),
+      ]),
+    );
   }
 }
---
+++ project_examples/lib/widgets/launch_image_widget.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:project_base_libs_pkg/base_file_headers.dart';
+import 'package:project_base_libs_pkg/third_lib_get.dart';
+
+enum LaunchImageWidgetStatus {
+  idle,
+  start,
+  didHide,
+}
+
+class LaunchImageWidget extends StatefulWidget {
+  const LaunchImageWidget({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => LaunchImageWidgetState();
+}
+
+class LaunchImageWidgetState extends State<LaunchImageWidget> {
+  final status = LaunchImageWidgetStatus.idle.obs;
+
+  @override
+  void initState() {
+    super.initState();
+    Util.delayedMilliseconds(1500, () => status.value = LaunchImageWidgetStatus.start);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      switch (status.value) {
+        case LaunchImageWidgetStatus.idle:
+          return AnimatedOpacity(opacity: 1, duration: Duration.zero, child: _widgetText());
+
+        case LaunchImageWidgetStatus.start:
+          return AnimatedOpacity(
+            opacity: 0,
+            duration: const Duration(milliseconds: 250),
+            child: _widgetText(),
+            onEnd: () => status.value = LaunchImageWidgetStatus.didHide,
+          );
+
+        case LaunchImageWidgetStatus.didHide:
+          return const SizedBox();
+      }
+    });
+  }
+
+  Widget _widgetText() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      alignment: Alignment.center,
+      color: Colors.white,
+      child: WidgetsFactory.text('YouXianMing', fontSize: 40.sp, color: Colors.blueAccent),
+    ).addDefaultTextStyle();
+  }
+}
