c15d7b8dd80b6f25edb5a81e7c3daaebc4ad3f3a
YouXianMing <YouXianMing1987@icloud.com>
2022-09-15 10:15:42 +0800

    map_uri_creator工具更新

--- project_base_libs_pkg/lib/base/tools/map_uri_creator.dart
+++ project_base_libs_pkg/lib/base/tools/map_uri_creator.dart
@@ -8,10 +8,10 @@ class MapUriCreator {
   /// 根据给定的地址字符串创建uri
   /// Returns a URL that can be launched on the current platform
   /// to open a maps application showing the result of a search query.
-  static String createQueryUrl(String query) {
+  static String createQueryUrl(String query, {bool useWebTypeUrl = false}) {
     late Uri uri;
 
-    if (kIsWeb) {
+    if (kIsWeb || useWebTypeUrl) {
       uri = Uri.https('www.google.com', '/maps/search/', {'api': '1', 'query': query});
     } else if (Platform.isAndroid) {
       uri = Uri(scheme: 'geo', host: '0,0', queryParameters: {'q': query});
@@ -27,22 +27,18 @@ class MapUriCreator {
   /// 根据给定的经纬度创建uri
   /// Returns a URL that can be launched on the current platform
   /// to open a maps application showing coordinates ([latitude] and [longitude]).
-  static String createCoordinatesUrl(double latitude, double longitude, [String? label]) {
+  static String createCoordinatesUrl(double latitude, double longitude, {String? label, bool useWebTypeUrl = false}) {
     late Uri uri;
 
-    if (kIsWeb) {
+    if (kIsWeb || useWebTypeUrl) {
       uri = Uri.https('www.google.com', '/maps/search/', {'api': '1', 'query': '$latitude,$longitude'});
     } else if (Platform.isAndroid) {
       var query = '$latitude,$longitude';
-
       if (label != null) query += '($label)';
-
       uri = Uri(scheme: 'geo', host: '0,0', queryParameters: {'q': query});
     } else if (Platform.isIOS) {
       var params = {'ll': '$latitude,$longitude'};
-
       if (label != null) params['q'] = label;
-
       uri = Uri.https('maps.apple.com', '/', params);
     } else {
       uri = Uri.https('www.google.com', '/maps/search/', {'api': '1', 'query': '$latitude,$longitude'});
