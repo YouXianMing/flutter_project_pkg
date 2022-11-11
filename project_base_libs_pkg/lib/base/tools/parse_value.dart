ece31141afa504fdc799c2e42863dd74d52e143e
YouXianMing <YouXianMing1987@icloud.com>
2022-11-11 15:55:06 +0800

    修改parse_value工具类

--- project_base_libs_pkg/lib/base/tools/parse_value.dart
+++ project_base_libs_pkg/lib/base/tools/parse_value.dart
@@ -1,26 +1,34 @@
-/// 尝试将数据int或者double类型数据转换为double类型
+/// 尝试将数据类型int,double,bool,string转换为double类型
 double? tryParseToDouble(dynamic value) {
   if (value is double) {
     return value;
   } else if (value is int) {
     return value.toDouble();
+  } else if (value is bool) {
+    return (value == true ? 1.0 : 0.0);
+  } else if (value is String) {
+    return double.tryParse(value);
   } else {
     return null;
   }
 }
 
-/// 尝试将数据int或者double类型数据转换为int类型
+/// 尝试将数据类型int,double,bool,string转换为int类型
 int? tryParseToInt(dynamic value) {
   if (value is int) {
     return value;
   } else if (value is double) {
     return value.toInt();
+  } else if (value is bool) {
+    return (value == true ? 1 : 0);
+  } else if (value is String) {
+    return int.tryParse(value);
   } else {
     return null;
   }
 }
 
-/// 尝试将数据bool,double,或者int转换为bool类型
+/// 尝试将数据类型bool,int,double,string转换为bool类型
 bool? tryParseToBool(dynamic value) {
   if (value is bool) {
     return value;
@@ -28,6 +36,15 @@ bool? tryParseToBool(dynamic value) {
     return value != 0;
   } else if (value is double) {
     return value != 0.0;
+  } else if (value is String) {
+    var tmp = value.toLowerCase();
+    if (tmp == 'true') {
+      return true;
+    } else if (tmp == 'false') {
+      return false;
+    } else {
+      return null;
+    }
   } else {
     return null;
   }
