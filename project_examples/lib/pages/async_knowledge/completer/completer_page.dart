cdd9415d46d6a8554cbcee70a3309bd1c5ea421a
YouXianMing <YouXianMing1987@icloud.com>
2023-03-07 16:58:58 +0800

    更新project_examples项目

--- project_examples/lib/pages/async_knowledge/completer/completer_page.dart
+++ project_examples/lib/pages/async_knowledge/completer/completer_page.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:project_base_libs_pkg/base/widgets/widgets_factory/widgets_factory.dart';
 import 'package:project_examples/base/normal_stateful_widget.dart';
 import 'package:project_examples/route/app_route_manager.dart';
 import 'package:project_examples/widgets/custom_app_bar.dart';
 import 'package:project_base_libs_pkg/base/tools/util.dart';
 import 'package:project_base_libs_pkg/base/widgets/base_stateful_widget.dart';
-import 'package:project_base_libs_pkg/base/widgets/widgets_factory.dart';
 import 'package:project_examples/app_route_style.dart';
 
 class CompleterPage extends NormalStatefulWidget {
